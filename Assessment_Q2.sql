d821844ddad176989bf5e0924944c5def15cbc05
PreciousChizurum <preciouscogbusu@gmail.com>
2025-05-19 15:34:49 +0100

    Update Assessment_Q2.sql

--- Assessment_Q2.sql
+++ Assessment_Q2.sql
@@ -1,36 +1,40 @@
 -- Question 2: Transaction Frequency Analysis
--- Calculate the average number of transactions per customer per month and categorize them by their transaction frequency
+-- Calculate the average number of transactions of each customer per month and categorize them by their transaction frequency
 
+-- Calculate the number of transactions monthly 
 WITH monthly_transactions AS (
     SELECT 
         s.owner_id,
         COUNT(*) AS transaction_count,
-        COUNT(DISTINCT DATE_TRUNC('month', s.created_at)) AS months_active
+        EXTRACT(MONTH FROM s.transaction_date) AS month,
     FROM 
         savings_savingsaccount s
-    WHERE 
-        s.confirmed_amount > 0  -- Only count actual transactions
     GROUP BY 
-        s.owner_id
+        s.owner_id, EXTRACT(MONTH FROM s.transaction_date)
 ),
-frequency_analysis AS (
+
+-- Calculate the average number of transactions
+customer_avg AS (
     SELECT 
         owner_id,
-        CASE 
-            WHEN transaction_count/months_active >= 10 THEN 'High Frequency'
-            WHEN transaction_count/months_active >= 3 THEN 'Medium Frequency'
-            ELSE 'Low Frequency'
-        END AS frequency_category,
-        transaction_count/months_active AS avg_transactions_per_month
+        AVG(transaction_count) AS avg_transactions_per_month
     FROM 
         monthly_transactions
+    GROUP BY 
+        owner_id
 )
+
+-- Categorize by frequency
 SELECT 
-    frequency_category,
-    COUNT(owner_id) AS customer_count,
+    CASE 
+        WHEN avg_transactions_per_month >= 10 THEN 'High Frequency'
+        WHEN avg_transactions_per_month >= 3 AND avg_transactions_per_month <= 9 THEN 'Medium Frequency'
+        ELSE 'Low Frequency'
+    END AS frequency_category,
+    COUNT(*) AS customer_count,
     ROUND(AVG(avg_transactions_per_month), 1) AS avg_transactions_per_month
 FROM 
-    frequency_analysis
+    customer_avg
 GROUP BY 
     frequency_category
 ORDER BY 
